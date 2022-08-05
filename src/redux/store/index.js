018da5b3b1f3208fcb95c12ff59ac59cb9a2338c
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-08-05 15:36:33 -0300

    cria arquivos do redux

---
+++ src/redux/store/index.js
@@ -0,0 +1,13 @@
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunk),
+));
+
+if (window.Cypress) {
+  window.store = store;
+}
+
+export default store;
