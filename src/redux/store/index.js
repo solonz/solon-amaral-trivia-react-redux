9483e66e34b7f71fd3b67542b49b1dfae7ad3d46
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-08-05 15:19:59 -0300

    corrige a store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux';
-import { composeWithDevTools } from '@redux-devtools/extension';
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(rootReducer, composeWithDevTools());
+
+if (window.Cypress) {
+  window.store = store;
+}
 
 export default store;
