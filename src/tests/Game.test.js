13d8a1af9dedc28bbb332020a6406755761de7a8
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-08-10 11:51:43 -0300

    corrige o teste do game e atinge 95% de coverage

--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -1,5 +1,5 @@
 import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux";
-import { screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { screen, waitForElementToBeRemoved, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import App from "../App";
@@ -7,6 +7,7 @@ import App from "../App";
 describe('Testes do game',  () => {
   test('testa se os itens estão na tela' +
   'e se o timer volta o countdown no 30', async () => {
+
    renderWithRouterAndRedux(<App />);
 
     const emailInput = screen.getByTestId('input-gravatar-email');
@@ -40,5 +41,34 @@ describe('Testes do game',  () => {
     userEvent.click(next);
     expect(timer.innerHTML).toBe('30');
 
+
+    
 });
+test('', async ()=>{
+    jest.useFakeTimers();
+
+    renderWithRouterAndRedux(<App />);
+
+    const emailInput = screen.getByTestId('input-gravatar-email');
+    const nameInput = screen.getByTestId('input-player-name');
+    const playButton = screen.getByTestId('btn-play');
+
+    userEvent.type(emailInput, 'email@gmail.com');
+    userEvent.type(nameInput, 'name');
+    userEvent.click(playButton);
+
+    await waitForElementToBeRemoved(nameInput);
+
+    const loading = screen.getByTestId('loading');
+    
+    await waitForElementToBeRemoved(loading);
+    const correct_answer = screen.getByTestId('correct-answer');
+
+    await waitFor(()=>{
+      jest.advanceTimersByTime(31000);
+      expect(correct_answer).toBeDisabled();
+    })
+
+
+})
 });
